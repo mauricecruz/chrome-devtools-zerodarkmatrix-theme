837effb74f9159170d778800f82d9ead39f727b4
Maurice Cruz <maurice.cruz@gmail.com>
2015-06-03 18:19:02 -0400

    follow https://github.com/mauricecruz/zero-base-themes for updates

--- theme-extension/devtools.js
+++ theme-extension/devtools.js
@@ -1,8 +1,8 @@
 var xhr = new XMLHttpRequest(),
 	stylesheet = 'stable.css';
 
-if (/Chrome\/(\d\d)/.exec(navigator.userAgent)[1] > 36) {
-	stylesheet = 'canary.css'
+if (/Chrome\/(\d\d)/.exec(navigator.userAgent)[1] > 43) {
+	stylesheet = 'canary.css';
 }
 
 xhr.open("GET", "/" + stylesheet, false);
